614ef892e00af5be209b2607d368b0186bc3ae9a
notgoodatpickingnames <32577550+notgoodatpickingnames@users.noreply.github.com>
2020-03-25 22:31:42 -0500

    no

--- EmployeeScheduler/src/app/scheduling/models/month.ts
+++ EmployeeScheduler/src/app/scheduling/models/month.ts
@@ -1,33 +1,49 @@
 import { Week } from "./week";
 import { DatePipe } from "@angular/common";
+import { Shift } from "~/app/core/services/shift/shift";
+import { Observable } from "rxjs";
+import { SubscriptionBase } from "~/app/core/subscriptionBase";
+import { takeUntil } from "rxjs/operators";
+import { RecurrenceType } from "~/app/core/services/shift/recurrenceType";
 
-export class Month {
+export class Month extends SubscriptionBase {
     public weeks: Week[] = [];
-    public month: number;
+    public monthNumber: number;
     public year: number;
     public name: string;
 
     private datePipe = new DatePipe('en');
 
-    constructor(year: number, month: number) {
-        this.month = month;
-        this.year = year;
-        this.name = this.datePipe.transform(new Date(year, month, 1), 'MMMM');
+    private weeklyShifts: Shift[];
 
+    constructor(year: number, monthNumber: number, shift$: Observable<Shift[]>) {
+        super();
+
+        this.monthNumber = monthNumber;
+        this.year = year;
+        this.name = this.datePipe.transform(new Date(year, monthNumber, 1), 'MMMM');
+        this.listenForShifts(shift$);
         this.buildWeeks();
     }
 
     public get numberOfDays() {
-        return new Date(this.year, this.month + 1, 0).getDate();
+        return new Date(this.year, this.monthNumber + 1, 0).getDate();
     }
 
     private buildWeeks() {
-        const firstOfTheMonth = new Date(this.year, this.month, 1);
-        const firstSunday = new Date(this.year, this.month, firstOfTheMonth.getDate() - firstOfTheMonth.getDay());
+        const firstOfTheMonth = new Date(this.year, this.monthNumber, 1);
+        const firstSunday = new Date(this.year, this.monthNumber, firstOfTheMonth.getDate() - firstOfTheMonth.getDay());
 
         for (let i = 0; i < this.numberOfDays; i += 7) {
             this.weeks.push(new Week(new Date(firstSunday.getFullYear(), firstSunday.getMonth(), firstSunday.getDate() + i)));
         }
+    }
+
+    private listenForShifts(shift$: Observable<Shift[]>): void {
+        shift$.pipe(takeUntil(this.componentDestroyed)).subscribe(shifts => {
+            this.weeklyShifts = shifts.filter(shift => shift.recurrenceType === RecurrenceType.EveryWeek);
+
+        });
     }
 
 }
--- EmployeeScheduler/src/app/scheduling/scheduling.component.ts
+++ EmployeeScheduler/src/app/scheduling/scheduling.component.ts
@@ -1,24 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { Month } from './models/month';
+import { ShiftsService } from '../core/services/shift/shifts.service';
+import { SubscriptionBase } from '../core/subscriptionBase';
+import { Shift } from '../core/services/shift/shift';
 
 @Component({
     selector: 'ns-scheduling',
     templateUrl: './scheduling.component.html',
     styleUrls: ['./scheduling.component.css']
 })
-export class SchedulingComponent {
+export class SchedulingComponent extends SubscriptionBase {
     private today = new Date();
-    public month = new Month(this.today.getFullYear(), this.today.getMonth());
+    private shifts: Shift[] = [];
+    public month = new Month(this.today.getFullYear(), this.today.getMonth(), this.shifts);
+
+    constructor(private shiftService: ShiftsService) {
+        super();
+
+    }
 
     public get nameOfMonth(): string {
         return this.month.name;
     }
 
     public onNextMonthClick() {
-        this.month = new Month(this.month.year, this.month.month + 1);
+        this.month = new Month(this.month.year, this.month.monthNumber + 1, this.shifts);
     }
 
     public onPreviousMonthClick() {
-        this.month = new Month(this.month.year, this.month.month - 1);
+        this.month = new Month(this.month.year, this.month.monthNumber - 1, this.shifts);
+    }
+
+    private listenForShifts(): void {
+        this.shiftService.shift$
     }
 }
