cedc0fc120200f9d662bf410ae0396596d065cc1
notgoodatpickingnames <32577550+notgoodatpickingnames@users.noreply.github.com>
2020-09-23 22:41:23 -0500

    sync

--- scheduling-app/src/app/core/services/user/users.service.ts
+++ scheduling-app/src/app/core/services/user/users.service.ts
@@ -10,13 +10,46 @@ export class UsersService {
     public user$: BehaviorSubject<User>;
 
     private _user: User;
-
-
+    private _userPath: string = "users";
 
     private setRelatedStoreIds(userId: string) {
         firebase.setValue(`${this._userPath}/${userId}/relatedStores`, this.relatedStoreIds)
             .then(() => {
                 this.getStoreListeners();
             });
+    }
+
+    private getStoreListener(storeId: string): Observable<any> {
+        return new Observable((observer: any) => {
+
+            const onValueEvent =(snapshot: any) => {
+                this._ngZone.run(() => {
+                    const results = this.handleSnapshot(snapshot.value, storeId);
+                    observer.next(results);
+                })
+            }
+            firebase.addValueEventListener(onValueEvent, `/${this._storePath}/${storeId}`);
+
+        })
+        .pipe(catchError(this.handleErrors));
+    }
+
+    private handleErrors(error: Response): Observable<never> {
+        return throwError(error);
+    }
+
+    public get(storeId: string): Promise<Store> {
+        return firebase.getValue(`${this._storePath}/${storeId}`)
+            .then(storeFromFirebase => new Store(storeFromFirebase['value'], storeId));
+    }
+
+    private handleSnapshot(data: any, storeId: string): User {
+        let store: Store;
+
+        if (data) {
+            store = new Store(data, storeId);
+        }
+
+        return store;
     }
 }
